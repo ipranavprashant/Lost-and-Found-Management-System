11c1ff0fbfe15b187880efda36b17209168368df
Pranav Prashant <pranavprashant@outlook.in>
2023-11-18 21:10:17 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+const navigate = useNavigate();
 
 
 const Navbar = () => {
