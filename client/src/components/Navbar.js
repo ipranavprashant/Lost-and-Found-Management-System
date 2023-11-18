17e5c4e7648f94320dad3b5c994e10423be1b93e
Pranav Prashant <pranavprashant@outlook.in>
2023-11-18 21:11:54 +0530

    Update Navbar.js

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
 
 
 const Navbar = () => {
+  
+const navigate = useNavigate();
+
   const [menuOpen, setMenuOpen] = useState(false);
   const [isSmallScreen, setIsSmallScreen] = useState(false);
   const isAuthenticated = localStorage.getItem('authToken');
